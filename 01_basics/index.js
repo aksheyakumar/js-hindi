c85b8348865773aaafa5b1e09cce8468a88c09d9
aksheyakumar <akshaygurjar28803@gmail.com>
2025-09-07 13:07:45 +0000

    datatypes

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,1 +1,10 @@
 console.log("aksheya kumar")
+const accountId = 124534
+let accountEmail = "akshaygurjar28803@gmail.com"
+var account_city = "chirawa"
+account_city = "jhunjhunu"
+
+// perfer not to use var 
+// because of issue in block scope and functional scope
+
+console.table([accountId,accountEmail,account_city])
---
+++ 02_datatype.js
@@ -0,0 +1,28 @@
+// treat all js code as newer version
+"use strict";
+
+// alert(3+3) //we are using nodejs,not browser
+
+console.log(3 +3) //code redability should be high
+
+console.log("aksheya kumar")
+
+let name = "aksheya kumar"
+let age = 21
+let isStudent = true
+
+// number => 2^53 itna bada number use krskte h
+// bigint ye bhi use kr skte h
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine => abhi value assign nhi hui h
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefine)  //type aavego undefine
+console.log(typeof null); //type aavego object
+
