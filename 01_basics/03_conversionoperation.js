3225906ae4ed3accace7d42bc0a1051441991b5c
aksheyakumar <akshaygurjar28803@gmail.com>
2025-09-08 12:37:19 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score = 33
+
+console.log(typeof (score));
+
+let valueNumber = Number(score)
+console.log(typeof (valueNumber));
+console.log(valueNumber);
+// number conversion
+// "33" = 33 hi aayega
+// "33abc" => nan(not a number)
+// true => 1;false=>0
+// null => 0
+// undefine => nan
+
+// boolean conversion
+// 1 => true;0 =>false
+// "" => false
+// "aksheya" => true
+
+// string conversion 
+// 33 aayega toh 33 pr type string ho jaayega
